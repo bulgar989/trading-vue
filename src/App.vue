bf03f6f9cada8ec7be7b268b79aee6579d63d6e0
rinat989 <bulgar989@yandex.ru>
2022-03-25 10:18:28 +0300

    trading

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <trading-vue :data="this.$data"></trading-vue>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TradingVue from 'trading-vue-js'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { TradingVue },
+  data() {
+    return {
+      ohlcv: [
+        [ 1551128400000, 33,  37.1, 14,  14,  196 ],
+        [ 1551132000000, 13.7, 30, 6.6,  30,  206 ],
+        [ 1551135600000, 29.9, 33, 21.3, 21.8, 74 ],
+        [ 1551139200000, 21.7, 25.9, 18, 24,  140 ],
+        [ 1551142800000, 24.1, 24.1, 24, 24.1, 29 ],
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
